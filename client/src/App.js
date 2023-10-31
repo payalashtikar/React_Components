3b480b01c0a3dbcac5b4330816417edd660a59de
unknown <payalashtikar@gmail.com>
2023-10-31 14:47:54 +0530

    image-uploader-component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import DarkTheme from './dark-them-component/DarkTheme';
 import Box from './flip color of boxes/Box';
 import Hide_Show from './hide-and-show-content/Hide_Show';
-import ImageUploading from './image-upload/ImageUploading';
+import ImageUploading from './image-upload/ImageUpload';
+// import ImageUploading from './image-upload/ImageUploading';
 import PaginationComponent from './pagination-component/PaginationComponent';
 import SearchComponent from './search-component/SearchComponent';
 import Sidebar from './sidebar component/Sidebar';
